2f7021fc34ce866b40ae06df2f071ab17baa44ef
Artūras Bušininkas <arturasbus@gmail.com>
2019-02-19 00:28:39 +0200

    bubbles done

--- js/app.js
+++ js/app.js
@@ -1,8 +1,6 @@
 
 function closeSide(){
     document.getElementById('side').style.left = '100%';
-  
-    
 }
 
 //Media Query function
@@ -10,15 +8,29 @@ function mediaQuery(){
     if (window.matchMedia("(max-width: 500px)").matches){
 
         document.getElementById('side').style.left = '0';
-        document.getElementById('container').style.transform = 'translateX(-100%)';
 
     }else if (window.matchMedia("(max-width: 767px)").matches){
 
         document.getElementById('side').style.left = '20%';
-        document.getElementById('container').style.transform = 'translateX(-100%)';
     }
     else{
-        document.getElementById('side').style.left = '50%';
-        document.getElementById('container').style.transform = 'translateX(-100%)';      
+        document.getElementById('side').style.left = '50%';     
     }
+}
+
+// When the user scrolls down 20px from the top of the document, show the button
+document.getElementById('myBtn').onscroll = function() {scrollFunction()};
+
+function scrollFunction() {
+  if (window.pageYOffset || document.documentElement.scrollTop) {
+    document.getElementById("myBtn").style.display = "block";
+  } else {
+    document.getElementById("myBtn").style.display = "none";
+  }
+}
+
+// When the user clicks on the button, scroll to the top of the document
+function topFunction() {
+  document.body.scrollTop = 0;
+  document.documentElement.scrollTop = 0;
 }
