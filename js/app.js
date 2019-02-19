deb7dad7987ce2b5af13e10c97612e48bca2466c
Arturas Busininkas <arturasbus@gmail.com>
2019-02-19 19:31:14 +0200

    Fully done

--- js/app.js
+++ js/app.js
@@ -1,36 +1,36 @@
 
+// const side = document.getElementById('side');
+// const btn = document.getElementById("topBtn");
+
+// // Back to top
+// btn.addEventListener("click", toTop);
+
+// function toTop(){
+//   side.scrollTop = 0;
+// }
+
+// Close side panel
 function closeSide(){
     document.getElementById('side').style.left = '100%';
+    document.getElementById('topBtn').style.display = 'none';
 }
 
-//Media Query function
+//Opens side panel depend on screen size
 function mediaQuery(){
     if (window.matchMedia("(max-width: 500px)").matches){
 
         document.getElementById('side').style.left = '0';
+        document.getElementById('topBtn').style.display = 'block'; 
 
     }else if (window.matchMedia("(max-width: 767px)").matches){
 
         document.getElementById('side').style.left = '20%';
+        document.getElementById('topBtn').style.display = 'block'; 
     }
     else{
-        document.getElementById('side').style.left = '50%';     
+        document.getElementById('side').style.left = '50%'; 
+        document.getElementById('topBtn').style.display = 'block';    
     }
 }
 
-// When the user scrolls down 20px from the top of the document, show the button
-document.getElementById('myBtn').onscroll = function() {scrollFunction()};
 
-function scrollFunction() {
-  if (window.pageYOffset || document.documentElement.scrollTop) {
-    document.getElementById("myBtn").style.display = "block";
-  } else {
-    document.getElementById("myBtn").style.display = "none";
-  }
-}
-
-// When the user clicks on the button, scroll to the top of the document
-function topFunction() {
-  document.body.scrollTop = 0;
-  document.documentElement.scrollTop = 0;
-}
